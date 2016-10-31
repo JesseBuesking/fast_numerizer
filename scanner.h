8897b9321e7b8573275255dec0f0c28d0efce3cc
JesseBuesking <jessebuesking@gmail.com>
2016-10-30 17:28:54 -0700

    include generated files by tools (just c code)

---
+++ scanner.h
@@ -0,0 +1,15 @@
+#ifndef NUMERIZER_SCANNER_H_
+#define NUMERIZER_SCANNER_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "scanner.def.h"
+#include "parser.h"
+#include "scan.h"
+#include "readmem.h"
+
+int numerizer_start(scanstate *ss);
+
+#endif // NUMERIZER_SCANNER_H_
